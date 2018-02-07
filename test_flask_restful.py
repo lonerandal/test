cd7e50f1263415fa46c53fb3726298063d24b126
Randal <randal@RandaldeMacBook-Pro.local>
2018-02-07 09:55:21 +0800

    change

---
+++ test_flask_restful.py
@@ -0,0 +1,14 @@
+from simple_flask import Flask
+from simple_flask.ext import restful
+
+app = Flask(__name__)
+api = restful.Api(app)
+
+class HelloWorld(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run()
